cf3dc67e2e86bb95b202ab1173dc1d2c9840cd00
najiha <najiha@users.noreply.github.com>
2016-09-28 13:25:41 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,125 @@
+  $(document).ready(function() {
+    (function() {
+        [].slice.call(document.querySelectorAll('.tabs')).forEach(function(el) {
+            new CBPFWTabs(el);
+        });
+    })();
+    $('#main-nav').sidr();
+    $('#fullpage').fullpage({
+        'verticalCentered': true,
+        'easing': 'easeInOutCirc',
+        'css3': false,
+        'scrollingSpeed': 600,
+        'slidesNavigation': true,
+        'slidesNavPosition': 'bottom',
+        'easingcss3': 'ease',
+        'navigation': true,
+        'anchors': ['Home', 'Features', 'About', 'Clients', 'Login', 'Pricing', 'Download', 'Contact'],
+        'navigationPosition': 'left'
+    });
+    $('.screenshots-content, .clients-content').css('height', $(window).height());
+
+    // CONTACT FORM
+
+  
+       $(document).mouseup(function (e) {
+    if ($(".sidr-open")[0]){
+    var container = $("#sidr");
+
+    if (!container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+      $(".sidr-open #main-nav").click();
+    }}
+});
+ 
+ 
+$('#submit').click(function(){
+ 
+$.post("contact.php", $("#contact-form").serialize(),  function(response) {
+$('#success').fadeIn().html(response);
+$('#success').delay(2000).fadeOut();
+});
+return false;
+ 
+});
+ 
+
+  });
+
+//  jQuery(window).load(function() {
+//    jQuery('#preloader').fadeOut();
+//  });
+jQuery(document).ready(function($) {  
+
+// site preloader -- also uncomment the div in the header and the css style for #preloader
+$(window).load(function(){
+	$('#preloader').fadeOut('slow',function(){$(this).remove();});
+});
+
+});
+//LOGIN
+$(document).ready(function() {
+  
+  var animating = false,
+      submitPhase1 = 1100,
+      submitPhase2 = 400,
+      logoutPhase1 = 800,
+      $login = $(".login"),
+      $app = $(".app");
+  
+  function ripple(elem, e) {
+    $(".ripple").remove();
+    var elTop = elem.offset().top,
+        elLeft = elem.offset().left,
+        x = e.pageX - elLeft,
+        y = e.pageY - elTop;
+    var $ripple = $("<div class='ripple'></div>");
+    $ripple.css({top: y, left: x});
+    elem.append($ripple);
+  };
+  
+  $(document).on("click", ".login__submit", function(e) {
+    if (animating) return;
+    animating = true;
+    var that = this;
+    ripple($(that), e);
+    $(that).addClass("processing");
+    setTimeout(function() {
+      $(that).addClass("success");
+      setTimeout(function() {
+        $app.show();
+        $app.css("top");
+        $app.addClass("active");
+      }, submitPhase2 - 70);
+      setTimeout(function() {
+        $login.hide();
+        $login.addClass("inactive");
+        animating = false;
+        $(that).removeClass("success processing");
+      }, submitPhase2);
+    }, submitPhase1);
+  });
+  
+  $(document).on("click", ".app__logout", function(e) {
+    if (animating) return;
+    $(".ripple").remove();
+    animating = true;
+    var that = this;
+    $(that).addClass("clicked");
+    setTimeout(function() {
+      $app.removeClass("active");
+      $login.show();
+      $login.css("top");
+      $login.removeClass("inactive");
+    }, logoutPhase1 - 120);
+    setTimeout(function() {
+      $app.hide();
+      animating = false;
+      $(that).removeClass("clicked");
+    }, logoutPhase1);
+  });
+  
+});
+
+
